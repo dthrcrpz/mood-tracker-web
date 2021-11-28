d91b53ca57df6af2db6dad7cdaa421da77820416
teryong <dthrcrpz@gmail.com>
2021-11-28 11:26:09 +0800

    added redirect when result is null

--- pages/result.vue
+++ pages/result.vue
@@ -18,6 +18,11 @@
             ...mapGetters({
                 result: 'track/getResults'
             })
+        },
+        mounted () {
+            if (this.result == null) {
+                this.$router.push('/')
+            }
         }
     }
 </script>
