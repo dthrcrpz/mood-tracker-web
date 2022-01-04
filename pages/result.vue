d300d53984cad00de8806d8ccad32a90ed811824
teryong <dthrcrpz@gmail.com>
2022-01-04 21:29:14 +0800

    removed result pre header

--- pages/result.vue
+++ pages/result.vue
@@ -2,7 +2,6 @@
     <div class="page-result">
         <div class="container">
             <div class="texts">
-                <p class="seems">It seems like you are</p>
                 <p class="result negative">{{ result.result }}</p>
             </div>
             <div class="assistance" v-html="result.remarks"></div>
